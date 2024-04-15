c989769956c146b5b28eddba1b54d0a66a246373
guerrero <arielguerrero1012@gmail.com>
2024-04-15 21:12:37 +0000

    feat: add more tests for deck

--- projects/cards-project/deck_test.go
+++ projects/cards-project/deck_test.go
@@ -12,3 +12,22 @@ func TestNewDeck(t *testing.T) {
 	}
 }
 
+func TestFaceCount(t *testing.T) {
+	d := newDeck()
+	for _, face := range cardFaces {
+		c := d.faceCount(face)
+		if c != 4 {
+			t.Errorf("Expected 4 cards with face %v, but got %v", face, c)
+		}
+	}		
+}
+
+func TestSuitCount(t *testing.T) {
+	d := newDeck()
+	for _, suit := range cardSuits {
+		c := d.suitCount(suit)
+		if c != 13 {
+			t.Errorf("Expected 13 cards with suit %v, but got %v", suit, c)
+		}
+	}
+}
